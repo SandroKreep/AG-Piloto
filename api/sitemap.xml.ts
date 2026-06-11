47d465f7825fca236319798f3b1ceeec19ec5657
Sandro Butler <sandrobutler284@gmail.com>
2026-06-11 21:06:38 +0100

    feat: sitemap via API route

---
+++ api/sitemap.xml.ts
@@ -0,0 +1,20 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://ag-piloto.vercel.app/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://ag-piloto.vercel.app/motoqueiro/pedidos</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>`
+
+  res.setHeader('Content-Type', 'application/xml')
+  res.status(200).send(sitemap)
+}
